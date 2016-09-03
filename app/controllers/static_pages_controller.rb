1c08098b7cab7a9fb9c47479a1990b2394dc7771
jrkinouani <kinouani@gmail.com>
2016-09-03 14:36:29 +0400

    add page infos

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,4 +4,7 @@ class StaticPagesController < ApplicationController
 
   def about
   end
+  def infos
+  	
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
 
   get  "/about"  =>  "static_pages#about"
 
+  get "/infos" => "static_pages#infos"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
